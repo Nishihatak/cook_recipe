941dc7a78f3b9c545d6b0538f3da5751fa1ce0be
Nishihatak <nishihata1451@gmail.com>
2020-02-07 13:40:03 +0900

    s3 image_submit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,16 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
-#   # 開発環境ではローカルに保存
-#     if Rails.env.development? || Rails.env.test?
-#       config.storage = :file
-#   # 本番はS3に保存
-#     elsif Rails.env.production?
-#       config.storage = :fog
-#       config.fog_provider = 'fog/aws'
-#       config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id:  ENV["AWS_ACCESS_KEY_ID"],
-#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#       region: 'ap-northeast-1'
-#     }
-#     config.fog_directory  = 'cookrecipe-s3'
-#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cookrecipe-s3'
-#     end
-#   end
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id:  ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+    config.fog_directory  = 'cook123r'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cook123r'
+    end
